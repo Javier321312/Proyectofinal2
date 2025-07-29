0c2f44d98ddcc8ac15641edab5536c99ea5beef1
Welkel <Welkel@DESKTOP-JT53L1T>
2025-07-28 22:51:45 -0400

    Base de datos conectada

--- Proyectofinal2/src/Conector/ConexionDB.java
+++ Proyectofinal2/src/Conector/ConexionDB.java
@@ -7,7 +7,7 @@ public class ConexionDB {
     public static void main(String[] args) {
          String url = "jdbc:mysql://localhost:3306/proyecto1";  
         String usuario = "root"; //  
-        String contrasena = "Kataverde321";  
+        String contrasena = "cKataverde321";  
 
         try {
              Connection conn = DriverManager.getConnection(url, usuario, contrasena);
